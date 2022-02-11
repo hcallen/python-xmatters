010b085d9b323d6020d678a3aafd0a307bfbe64c
hallie <hallieallen@gmail.com>
2022-02-10 20:27:51 -0800

    bump version

--- xmatters/__init__.py
+++ xmatters/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "1.0.6"
+# TODO: add kwargs options to calls with parameters
+
+__version__ = "1.0.7"
 
 from xmatters.session import XMSession
 from xmatters.utils import TokenFileStorage
