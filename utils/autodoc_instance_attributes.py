eafee732053bac31200d0328af47766984347cbb
hcallen <hallieallen@gmail.com>
2022-02-14 22:50:38 -0800

    add list types

--- utils/autodoc_instance_attributes.py
+++ utils/autodoc_instance_attributes.py
@@ -1,6 +1,6 @@
 import inspect
 import xmatters.objects
-import xmatters.objects.devices
+import xmatters.utils
 import re
 import os
 
@@ -13,6 +13,8 @@ def main():
     for module_name, module in inspect.getmembers(xmatters.objects, inspect.ismodule):
         for name, class_ in inspect.getmembers(module, inspect.isclass):
             class_names.append(name)
+    for name, class_ in inspect.getmembers(xmatters.utils, inspect.isclass):
+        class_names.append(name)
 
     for module_name, module in inspect.getmembers(xmatters.objects, inspect.ismodule):
 
@@ -20,27 +22,38 @@ def main():
         source_lines = inspect.getsourcelines(module)
         for line in source_lines[0]:
 
-            # check for instance attributes assigned from a dict
-            is_dict_assigned = re.match('\s+[^#](?P<attr_name>self\.[a-z_]+)\s=\s[a-z_]+\.get\(\'', line)
-            attr_name = is_dict_assigned.group('attr_name') if is_dict_assigned else None
-
-            # check for instance attributes assigned to a class
-            is_class_assigned_match = re.match('\s+[^#]self\.\w+\s=\s[a-z\.]*(?P<class_name>[A-Z]\w+)\(.*\)', line)
-            class_name = is_class_assigned_match.group('class_name').strip() if is_class_assigned_match else None
-            is_class_assigned = class_name in class_names
-
+            is_instance_attr = re.match('\s+[^#]self\.[a-z_]+\s=\s', line)
             has_comment = re.match('^.*\s+#:(?P<comment>.*)\n$', line)
-            comment_text = has_comment.group('comment').strip() if has_comment else None
+            has_comment_text = has_comment.group('comment').strip() if has_comment else None
 
-            if is_class_assigned or is_dict_assigned:
+            if is_instance_attr:
 
                 if not has_comment:
                     outline = line.rstrip() + '    #:'
                 else:
                     outline = line
 
-                if is_class_assigned and class_name and not comment_text:
-                    outline = outline.rstrip() + ' :class:`{}`'.format(class_name)
+
+                is_dict_assigned = re.match('\s+[^#](?P<attr_name>self\.[a-z_]+)\s=\s[a-z_]+\.get\(\'', line)
+                is_class_assigned = re.match('\s+[^#]self\.\w+\s=\s[a-z\.]*(?P<class_name>[A-Z]\w+)\(.*\)', line)
+                is_list_assigned = re.match('\s+[^#]self\.\w+\s=\s\[[a-z\.]*(?P<class_name>[A-Z]\w+)\(.*\).*\]', line)
+
+                if is_class_assigned:
+                    class_name = is_class_assigned.group('class_name').strip()
+                elif is_list_assigned:
+                    class_name = is_list_assigned.group('class_name').strip()
+                else:
+                    class_name = None
+
+                if class_name not in class_names:
+                    class_name = None
+
+                if not has_comment_text and not is_dict_assigned:
+
+                    if is_class_assigned:
+                        outline = outline.rstrip() + ' :class:`{}`'.format(class_name)
+                    elif is_list_assigned:
+                        outline = outline.rstrip() + ' [:class:`{}`]'.format(class_name)
 
                 outline = outline + '\n' if not outline.endswith('\n') else outline
 
